9cbaae1c3cd66ecc1ada59c7e5f31d3c5f8798ba
rajChaudharyCisco <149004488+rajChaudharyCisco@users.noreply.github.com>
2025-02-18 09:13:57 -0600

    Update check_sync.sh
    Added services check-sync

--- check_sync.sh
+++ check_sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 . /<path_to_ncsrc_file>/ncsrc
 python3 ~/Desktop/NSO/Python_Services_examples/auto_check_sync.py
+python3 ~/Desktop/NSO/Python_Services_examples/auto_services_check_sync.py
